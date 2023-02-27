dec466b8533f22a2af55038f02afe452ca8ff690
5307096108 <ahabadullah@gmail.com>
2023-02-27 14:19:57 -0800

    this is the first repo

---
+++ src/github/GithubTesting.java
@@ -0,0 +1,12 @@
+package github;
+
+public class GithubTesting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String a = "this is the frist project";
+System.out.println(a);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author ahmadiabadullah
+ *
+ */
+module Github {
+}
